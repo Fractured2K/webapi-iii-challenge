8de5b03caa147e731b940d4cf7a346df56818f99
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-20 11:56:48 -0700

    Handle create user endpoint

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,18 @@ const router = require("express").Router();
 const User = require("../data/helpers/userDb");
 
 // Create user
-router.post("/", async (req, res) => {});
+router.post("/", async (req, res) => {
+	try {
+		const user = req.body;
+
+		const newUser = await User.insert(user);
+		res.status(201).json(newUser);
+	} catch (err) {
+		res.status(500).json({
+			message: "Sorry, but there was an error creating that user"
+		});
+	}
+});
 
 // Get users
 router.get("/", async (req, res) => {
