208984daba1dc3b307f6bd6236e3aed46b578f03
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-20 11:52:29 -0700

    Handle get users endpoint

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,15 @@
 const router = require("express").Router();
 
-router.get("/", (req, res) => {});
+const User = require("../data/helpers/userDb");
+
+// Get users
+router.get("/", async (req, res) => {
+	try {
+		const users = await User.get();
+		res.status(200).json(users);
+	} catch (err) {
+		res.status(500).json({ message: "Sorry, we couldn't get users" });
+	}
+});
 
 module.exports = router;
