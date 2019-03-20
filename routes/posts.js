2276df5bd7846560ec6105af89c191b19ead0d2b
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-20 14:24:13 -0700

    Handle create post endpoint

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,19 @@
 const router = require("express").Router();
 
+const Post = require("../data/helpers/postDb");
+
 // Create post
-router.post("/", async (req, res) => {});
+router.post("/", async (req, res) => {
+	try {
+		const post = req.body;
+
+		const newPost = await Post.insert(post);
+		res.status(201).json(newPost);
+	} catch (err) {
+		res.status(500).json({
+			message: "Sorry, but there was an error creating that post"
+		});
+	}
+});
 
 module.exports = router;
