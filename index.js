a4e7fcb1d8159a85a497f6aa08f9fa2037926d9b
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-20 11:44:49 -0700

    Scaffold API file structure

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// code away!
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+
+// endpoints
+const userRoutes = require("./routes/users");
+const postRoutes = require("./routes/posts");
+
+// error handling middleware
+const userMiddleware = require("./middleware/users");
+const postMiddleware = require("./middleware/posts");
+
+const server = express();
+
+// middelware
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
+
+// route handling
+server.use("/api/users", userRoutes);
+server.use("/api/posts", postRoutes);
+
+server.listen(3001, () => {
+	console.log(`Server now running on http://localhost:3001`);
+});
