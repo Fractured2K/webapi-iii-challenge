6a9fd88534f3b75a68e51466ace29785ff8e06ed
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-20 14:33:08 -0700

    Handle get all posts endpoint

--- routes/posts.js
+++ routes/posts.js
@@ -21,6 +21,19 @@ router.post("/", checkPost, async (req, res) => {
 });
 
 // Get all posts
-router.post("/", async (req, res) => {});
+router.post("/", async (req, res) => {
+	try {
+		const posts = Post.get();
+
+		if (!posts)
+			return res.status(404).json({ message: "Sorry, no posts found" });
+
+		return res.status(200).json(post);
+	} catch (err) {
+		res.status(500).json({
+			message: "Sorry, there was a problem retrieving posts"
+		});
+	}
+});
 
 module.exports = router;
